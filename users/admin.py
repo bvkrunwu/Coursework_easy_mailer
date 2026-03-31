ae14930a106edcbab372f2e2f779d41bfacf9a07
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-04-01 01:38:20 +0300

    Register User model in admin panel with custom display, filtering, and search options

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "is_staff", "is_active", "date_joined")
+    list_editable = ("is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
+    exclude = ("password",)
