7bececec0531badc2fe3710b60761bd4c23bfcd0
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-04-06 01:16:17 +0300

    Configure server-side caching

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from mailing.apps import MailingConfig
 from mailing.views import (
@@ -29,20 +30,20 @@ from mailing.views import (
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path("", MainView.as_view(), name="main"),
-    path("contacts/", Contacts.as_view(), name="contacts"),
-    path("subscribers/", SubscriberListView.as_view(), name="subscriber_list"),
+    path("", cache_page(60)(MainView.as_view()), name="main"),
+    path("contacts/", cache_page(60)(Contacts.as_view()), name="contacts"),
+    path("subscribers/", cache_page(60)(SubscriberListView.as_view()), name="subscriber_list"),
     path("subscribers/<int:pk>/", SubscriberDetailView.as_view(), name="subscriber_detail"),
     path("subscribers/create/", SubscriberCreateView.as_view(), name="subscriber_create"),
     path("subscribers/<int:pk>/update/", SubscriberUpdateView.as_view(), name="subscriber_update"),
     path("subscribers/<int:pk>/delete/", SubscriberDeleteView.as_view(), name="subscriber_delete"),
     path("subscriber/block/<int:pk>/", BlockSubscriberView.as_view(), name="block_subscriber"),
-    path("messages/", MessageListView.as_view(), name="message_list"),
+    path("messages/", cache_page(60)(MessageListView.as_view()), name="message_list"),
     path("messages/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
     path("messages/create/", MessageCreateView.as_view(), name="message_create"),
     path("messages/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
     path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
-    path("campaigns/", CampaignListView.as_view(), name="campaign_list"),
+    path("campaigns/", cache_page(60)(CampaignListView.as_view()), name="campaign_list"),
     path("campaigns/<int:pk>/", CampaignDetailView.as_view(), name="campaign_detail"),
     path("campaigns/create/", CampaignCreateView.as_view(), name="campaign_create"),
     path("campaigns/<int:pk>/update/", CampaignUpdateView.as_view(), name="campaign_update"),
